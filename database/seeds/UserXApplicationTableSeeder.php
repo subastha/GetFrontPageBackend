77e5ecd05b9fef8fac63d1f7d0344a481901be8e
Shubha Shrestha <suba.stha@hotmail.com>
2019-12-25 20:15:48 -0600

    added user_x_application seeder

---
+++ database/seeds/UserXApplicationTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserXApplicationTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('user_x_application')->insert([
+            'user_id' => 1,
+            'application_id' => 1,
+            'access_granted' => true,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+        DB::table('user_x_application')->insert([
+            'user_id' => 2,
+            'application_id' => 1,
+            'access_granted' => true,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+        DB::table('user_x_application')->insert([
+            'user_id' => 3,
+            'application_id' => 1,
+            'access_granted' => true,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+    }
+}
